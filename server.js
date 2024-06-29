7d6498543e0c23c6f3742fdc11f6a8e36d7f3e41
joaodeco <murilomergner@gmail.com>
2024-06-29 00:36:24 -0300

    Opa

--- server.js
+++ server.js
@@ -16,23 +16,29 @@ let players = [
 // Simulação de personagens por reino
 const reinos = {
     planoTerreno: [
-        { nome: 'Scorpion', habilidades: 'Fogo', origem: 'Netherrealm' },
-        { nome: 'Sub Zero', habilidades: 'Gelo', origem: 'Earthrealm' }
+        { nome: 'Liu Kang', habilidades: 'Força, agilidade, reflexos e resistência sobre humanas, nunchaku, controle de fogo', origem: 'Plano Terreno' },
+        { nome: 'Kung Lao', habilidades: 'Força, agilidade, reflexos e resistência sobre humanas,chapéu com láminas, teleporte, poderes espirituais', origem: 'Plano Terreno' },
+        { nome: 'Johnny Cage', habilidades: 'Força, agilidade, reflexos e resistência sobre humanas, karate, estilo de luta inesperado, punhos', origem: 'Plano Terreno' },
+        { nome: 'Raiden', habilidades: 'Manipulação de raios a sua vontade, teleporte, bastão,voo', origem: 'Plano Terreno' },
+        { nome: 'Sonya Blade', habilidades: 'Força, agilidade, reflexos, energia rosa, armas de fogo em geral', origem: 'Plano Terreno' },
+        { nome: 'Kanshi Takashi', habilidades: 'Força, agilidade, reflexos, telesinesia, manipulação de seu reflexo espiritual, espada', origem: 'Plano Terreno' },
+        { nome: 'Jax Briggs', habilidades: 'Força, resistencia, braços de ferro que permitem ele atacar seus adversarios, armas de fogo ', origem: 'Plano Terreno' }
     ],
     submundo: [
-        { nome: 'Quan Chi', habilidades: 'Feitiçaria', origem: 'Netherrealm' },
-        { nome: 'Noob Saibot', habilidades: 'Sombra', origem: 'Netherrealm' }
+        { nome: 'Quan Chi', habilidades: 'Feitiçaria do submundo, magia necromanciosa, espadas duplas', origem: 'Netherrealm' },
+        { nome: 'Noob Saibot, Bi Ham', habilidades: 'Manipulaçao de sua sombra, foice em meia lua, teleporte', origem: 'Netherrealm' },
+        { nome: 'Scorpion, Hanzo Hasashi', habilidades: 'Usa uma kunai amarrada com uma corrente, manipulação de fogo do inferno, habilidades ninja, teleporte', origem: 'Netherrealm' }
     ],
     mundoExterior: [
-        { nome: 'Raiden', habilidades: 'Relâmpagos', origem: 'Heavens' },
-        { nome: 'Shao Kahn', habilidades: 'Conquistador', origem: 'Outworld' }
+        { nome: 'Sindel', habilidades: 'A capacidade de voar, manipular as ondas sonoras com seu grito sônico e usar seu cabelo comprido e letal nas lutas', origem: 'Outworld' },
+        { nome: 'Shao Kahn', habilidades: 'lançar bolas de fogo, criar portais, absorver almas, martelo de guerra e muiiiita resistencia', origem: 'Outworld' }
     ],
     reinoDoCaos: [
-        { nome: 'Havik', habilidades: 'Anarquia', origem: 'Chaosrealm' }
+        { nome: 'Havik', habilidades: 'Usa como quiser os seu membros para levar caos por onde passa', origem: 'Chaosrealm' }
     ],
     reinoDaOrdem: [
-        { nome: 'Kotal Kahn', habilidades: 'Sol', origem: 'Outworld' },
-        { nome: 'Skarlet', habilidades: 'Sangue', origem: 'Outworld' }
+        { nome: 'Kotal Kahn', habilidades: 'Poderes Solares, Sol', origem: 'Outworld' },
+        { nome: 'Skarlet', habilidades: 'Maga de Sangue', origem: 'Outworld' }
     ],
     edenia: [
         { nome: 'Kitana', habilidades: 'Levitação', origem: 'Edenia' },
@@ -40,7 +46,10 @@ const reinos = {
     ],
     dlc: [
         { nome: 'Spawn', habilidades: 'Demoníaco', origem: 'Hell' },
-        { nome: 'Rambo', habilidades: 'Armas', origem: 'Earthrealm' }
+        { nome: 'Rambo', habilidades: 'Armas', origem: 'Earthrealm' },
+        { nome: 'Omni-Man, Nolan Grayson', habilidades: 'Quase Invulnerabilidade. Cura Aprimorada. Capacidade Pulmonar Aprimorada.', origem: 'Série Invencivel' },
+        { nome: 'Homelander', habilidades: 'Voar, super força e super audição, pode ver através de quase tudo (exceto zinco) com a sua visão de raio X e depois destruí-lo com os seus olhos laser', origem: 'Serie The Boys' },
+        { nome: 'Pacificador', habilidades: 'Mestre em artes marciais, estrategista, armas de fogo', origem: 'DC Commics' }
     ]
 };
 
